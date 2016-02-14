c58884e76b7ff298fedfde0ac06404f896fb6c92
Michael Wei <michael@gfycat.com>
2016-02-13 21:27:02 -0800

    Installed devise, modified user registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # default URL I was instructed to add for using devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
